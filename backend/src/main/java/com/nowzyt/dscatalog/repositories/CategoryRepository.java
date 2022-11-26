7543134cab418c9f6b38592b994ec6d42740b3f3
Lucas Vinicius <howaitosame13@gmail.com>
2022-11-26 17:04:24 -0300

    connection h2 database

---
+++ backend/src/main/java/com/nowzyt/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.nowzyt.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nowzyt.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
