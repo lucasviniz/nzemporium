7ea475595eb5384194778635acfa343d7032295a
Lucas Vinicius <howaitosame13@gmail.com>
2023-04-28 12:42:53 -0300

    custom constraint validator

--- backend/src/main/java/com/nowzyt/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/nowzyt/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.nowzyt.dscatalog.dto;
 
+import com.nowzyt.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	private static final long serialVersionUID = 1L;
 	
