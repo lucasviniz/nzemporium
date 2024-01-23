f83833ffa3bc99cb77bfc7cbea387c4c54452879
Lucas <howaitosame13@gmail.com>
2024-01-22 21:46:09 -0300

    home page, added content image and text

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from 'components/Navbar';
+import Home from 'pages/Home';
 
 const App = () =>  {
   return (
     <>
-      <Navbar/>
+      <Home/>
     </>
   );
 }
