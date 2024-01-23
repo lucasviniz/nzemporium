fe35eeb594fbcc49bc132142d356bdca1d3da2ae
Lucas <howaitosame13@gmail.com>
2024-01-22 21:20:45 -0300

    navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,12 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
-function App() {
+const App = () =>  {
   return (
-   <h1>Hello NZEmporium</h1>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
