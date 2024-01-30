9380421f3da5f81fdd38a253bd3dc6efdcffe5bd
Lucas <howaitosame13@gmail.com>
2024-01-30 18:23:13 -0300

    home styling

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import Navbar from 'components/Navbar';
+import './styles.css'
+import ButtonIcon from 'components/ButtonIcon';
 
 const Home = () => {
   return (
@@ -9,9 +11,12 @@ const Home = () => {
         <div className="home-container">
             <div className="home-card">
                 <div className="home-content-container">
-                    <h1>Conheça o melhor catálogo de produtos</h1>
+                    <div>
+                      <h1>Conheça o melhor catálogo de produtos</h1>
+                      <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado</p>
+                    </div>
+                    <ButtonIcon/>
                 </div>
-
                 <div className="home-image-container">
                     <MainImage/>
                 </div>
